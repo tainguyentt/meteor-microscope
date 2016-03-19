41f3d7c325271ff0366feef633a26fa1a09817d6
Tai Nguyen <tai.tan849@gmail.com>
2016-03-20 01:19:18 +0700

    sorted posts by submitted time

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,16 +1,5 @@
-var postData = [{
-  title: 'Introducing Telescope',
-  url: 'http://sachagreif.com/introducing-telescope/'
-}, {
-  title: 'Meteor',
-  url: 'http://meteor.com'
-}, {
-  title: 'The Meteor Book',
-  url: 'http://themeteorbook.com'
-}];
-
 Template.postsList.helpers({
   posts: function() {
-    return Posts.find();
+    return Posts.find({}, { sort: { submitted: -1 } });
   }
 });
