1d27ead5cec5f80740f2358abb2ce57d9b03a727
Tai Nguyen <tai.tan849@gmail.com>
2016-03-19 17:14:08 +0700

    Added basic posts list template and static data

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,14 @@
+var postData = [{
+  title: 'Introducing Telescope',
+  url: 'http://sachagreif.com/introducing-telescope/'
+}, {
+  title: 'Meteor',
+  url: 'http://meteor.com'
+}, {
+  title: 'The Meteor Book',
+  url: 'http://themeteorbook.com'
+}];
+
+Template.postsList.helpers({
+  posts: postData
+});
